794e696477702dba77deffe8312b1713dc41653c
Yasirwan <afereviewer5@gmail.com>
2024-04-16 16:06:19 +0700

    5

--- Client/src/BackendURL.js
+++ Client/src/BackendURL.js
@@ -1,7 +1,7 @@
 // const BackendURL = "https://lmsbackend-020w.onrender.com"
 
 // test ini
-// const BackendURL = "http://localhost:4500"
+const BackendURL = "http://localhost:4500"
 //kalo ga bisa cba pake 127.0.0.1
-const BackendURL = "http://127.0.0.1:4500"
+// const BackendURL = "http://127.0.0.1:4500"
 export default BackendURL;
