1fdcf46234882b4ecf355b1c9b3910184eba81f0
Tsalsabilla <tsalsabilla569@gmail.com>
2024-04-16 13:08:00 +0700

    bcrypt

--- Server/routes/Admins.Route.js
+++ Server/routes/Admins.Route.js
@@ -30,50 +30,65 @@ router.post("/register", isAdminAuthenticated, async (req, res) => {
     if (user.length > 0) {
       return res.send({ msg: "User already registered" });
     }
-    bcrypt.hash(
+
+    const admin = new AdminModel({
+      name,
+      email,
       password,
-      +process.env.Salt_rounds,
-      async (err, secure_password) => {
-        if (err) {
-          console.log(err);
-        } else {
-          const admin = new AdminModel({
-            name,
-            email,
-            password: secure_password,
-          });
-          await admin.save();
-          let newAdmin = await AdminModel.find({ email });
+    });
 
-          const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-              user: "agrawaljoy1@gmail.com",
-              pass: "nsziioprjzwcodlm",
-            },
-          });
+    await admin.save();
+    let newAdmin = await AdminModel.find({ email });
 
-          const mailOptions = {
-            from: "agrawaljoy1@gmail.com",
-            to: email,
-            subject: "Account ID and Password",
-            text: `Welcome to LMS, Congratulations,Your account has been created successfully.This is your User type : Admin and Password : ${password}  `,
-          };
+    res.send({
+      msg: "Admin Registered Successfully",
+      admin: newAdmin[0],
+    });
 
-          transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              return res.send({ msg: "error" });
-            }
-            res.send({ msg: "Password sent" });
-          });
+    // bcrypt.hash(
+    //   password,
+    //   +process.env.Salt_rounds,
+    //   async (err, secure_password) => {
+    //     if (err) {
+    //       console.log(err);
+    //     } else {
+    //       const admin = new AdminModel({
+    //         name,
+    //         email,
+    //         password: secure_password,
+    //       });
+    //       await admin.save();
+    //       let newAdmin = await AdminModel.find({ email });
 
-          res.send({
-            msg: "Admin Registered Successfully",
-            admin: newAdmin[0],
-          });
-        }
-      }
-    );
+    //       const transporter = nodemailer.createTransport({
+    //         service: "gmail",
+    //         auth: {
+    //           user: "agrawaljoy1@gmail.com",
+    //           pass: "nsziioprjzwcodlm",
+    //         },
+    //       });
+
+    //       const mailOptions = {
+    //         from: "agrawaljoy1@gmail.com",
+    //         to: email,
+    //         subject: "Account ID and Password",
+    //         text: `Welcome to LMS, Congratulations,Your account has been created successfully.This is your User type : Admin and Password : ${password}  `,
+    //       };
+
+    //       transporter.sendMail(mailOptions, (error, info) => {
+    //         if (error) {
+    //           return res.send({ msg: "error" });
+    //         }
+    //         res.send({ msg: "Password sent" });
+    //       });
+
+    //       res.send({
+    //         msg: "Admin Registered Successfully",
+    //         admin: newAdmin[0],
+    //       });
+    //     }
+    //   }
+    // );
   } catch (err) {
     res.status(404).send({ msg: "Admin Registration failed" });
   }
@@ -88,22 +103,34 @@ router.post("/login", async (req, res) => {
       if (admin[0].access == "false") {
         return res.send({ message: "Access Denied" });
       }
-      bcrypt.compare(password, admin[0].password, (err, results) => {
-        if (results) {
-          let token = jwt.sign(
-            { email, name: admin[0].name },
-            process.env.secret_key,
-            { expiresIn: "7d" }
-          );
-          res.send({
-            message: "Login Successful",
-            user: admin[0],
-            token,
-          });
-        } else {
-          res.status(201).send({ message: "Wrong credentials" });
-        }
-      });
+      if (password === admin[0].password) {
+        let token = jwt.sign(
+          { email, name: admin[0].name },
+          process.env.secret_key,
+          { expiresIn: "7d" }
+        );
+        res.send({
+          message: "Login Successful",
+          user: admin[0],
+          token,
+        });
+      }
+      // bcrypt.compare(password, admin[0].password, (err, results) => {
+      //   if (results) {
+      //     let token = jwt.sign(
+      //       { email, name: admin[0].name },
+      //       process.env.secret_key,
+      //       { expiresIn: "7d" }
+      //     );
+      //     res.send({
+      //       message: "Login Successful",
+      //       user: admin[0],
+      //       token,
+      //     });
+      //   } else {
+      //     res.status(201).send({ message: "Wrong credentials" });
+      //   }
+      // });
     } else {
       res.send({ message: "Wrong credentials" });
     }
